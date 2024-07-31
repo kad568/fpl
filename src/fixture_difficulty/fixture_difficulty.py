ec0c394675ea5ee0a1841af70f9f3088a9291a38
kad568 <kadmiel.m5@gmail.com>
2024-07-31 22:19:59 +0100

    added heatmap by stat #3

--- src/fixture_difficulty/fixture_difficulty.py
+++ src/fixture_difficulty/fixture_difficulty.py
@@ -6,6 +6,7 @@ import seaborn as sns
 
 PATH_TO_HERE = Path(__file__).parent.resolve()
 
+
 def get_team_fpl_team_data():
     path_to_fpl_json = PATH_TO_HERE / "../fpl_data/fpl_data.json"
     
@@ -18,9 +19,7 @@ def get_team_fpl_team_data():
 
     return team_data
 
-def fpl_team_defensive_rating():
-
-    team_data = get_team_fpl_team_data()
+def plot_fpl_team_rating(team_data: pd.DataFrame):
 
     team_data = team_data[["code", "name", "strength", 'strength_overall_home', 'strength_overall_away',
        'strength_attack_home', 'strength_attack_away', 'strength_defence_home',
@@ -36,7 +35,7 @@ def fpl_team_defensive_rating():
         col_data_normalised = ( col_data - 1040 ) / ( 1370 - 1040 )
         team_data[col] = col_data_normalised
 
-    # sort by average rnk
+    # sort by average rank
     row_avg = team_data[['strength_overall_home', 'strength_overall_away']].mean(axis=1)
     row_order = row_avg.sort_values(ascending=False).index
     team_data = team_data.loc[row_order]
@@ -51,10 +50,46 @@ def fpl_team_defensive_rating():
     plt.tight_layout()
     plt.show()
 
+def plot_team_home_away_comparison(team_data: pd.DataFrame):
+
+    team_data = team_data[["code", "name", "strength", 'strength_overall_home', 'strength_overall_away',
+       'strength_attack_home', 'strength_attack_away', 'strength_defence_home',
+       'strength_defence_away']] 
+    
+    # normalize values
+    colomns_to_normalize = ['strength_overall_home', 'strength_overall_away',
+       'strength_attack_home', 'strength_attack_away', 'strength_defence_home',
+       'strength_defence_away']
+    
+    for col in colomns_to_normalize:
+        col_data = team_data[col]
+        col_data_normalised = ( col_data - 1040 ) / ( 1370 - 1040 )
+        team_data[col] = col_data_normalised
+
+    # avg by col
+    avg_by_col = team_data[['strength_overall_home', 'strength_overall_away',
+       'strength_attack_home', 'strength_attack_away', 'strength_defence_home',
+       'strength_defence_away']].mean(axis=0)
+    
+
+    df = pd.DataFrame(avg_by_col)
+    df.reset_index(inplace=True)
+    df.rename(columns={"index": "stat", 0: "score"}, inplace=True)
+    df.set_index("stat", inplace=True)
+
+
+    sns.heatmap(df, annot=True)
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+
 
 def main():
 
-    fpl_team_defensive_rating()
+    team_data = get_team_fpl_team_data()
+    
+    plot_fpl_team_rating(team_data)
+    # plot_team_home_away_comparison(team_data)
 
 
 
